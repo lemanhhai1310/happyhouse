9b1a1c88f251bc2f21777b99b5d45b297f273236
hailm <hai.lemanh.1310@gmail.com>
2023-07-23 10:04:47 +0700

    feat: layout footer

--- template-parts/layouts/footer.php
+++ template-parts/layouts/footer.php
@@ -1,3 +1,62 @@
+<!--footer-->
+<footer class="footer uk-light">
+    <div class="footer__top uk-section">
+        <div class="uk-container">
+            <div class="uk-grid uk-child-width-expand@m" uk-toggle="cls: uk-grid-large; mode: media; media: @m" uk-grid>
+                <div class="uk-width-auto@m">
+                    <div class="footer__width1">
+                        <div class="footer__logoTxt">Happy House</div>
+                        <div class="uk-flex uk-flex-column footer__list">
+                            <div class="footer__txt1">
+                                <span>Email:</span> admin@happyhouse.net
+                            </div>
+                            <div class="footer__txt1">
+                                <span>Hotline:</span> 0903246892
+                            </div>
+                            <div class="footer__txt1">
+                                <span>Website:</span> Happyhouse.net
+                            </div>
+                            <div class="uk-flex footer__txt1 uk-flex-middle" style="column-gap: 20px">
+                                <div><span>Kết nối với chúng tôi:</span></div>
+                                <div class="uk-flex" style="column-gap: 12px">
+                                    <a href=""><img src="images/iconFB.svg" alt=""></a>
+                                    <a href=""><img src="images/iconMessenger.svg" alt=""></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="">
+                    <div class="uk-flex uk-flex-column footer__list">
+                        <div class="footer__txt1">
+                            <span>Cơ sở 1:</span> Số 33, Trung Kính, Phường Trung Hòa, Quận Cầu Giấy, TP Hà Nội
+                        </div>
+                        <div class="footer__txt1">
+                            <span>Cơ sở 2:</span> Số 11, ngách 2, ngõ 535 Kim Mã, Ba Đình, Hà Nội
+                        </div>
+                        <div class="footer__txt1">
+                            <span>Cơ sở 3:</span> Số 5, lô 1 ô C4, Nam Trung Yên, Cầu Giấy, Hà nội
+                        </div>
+                        <div class="footer__txt1">
+                            <span>Cơ sở 4:</span> Số 28 đường Tiên Sơn, Thi Đua, Việt Trì, Phú Thọ
+                        </div>
+                    </div>
+                </div>
+                <div class="uk-width-auto@m">
+                    <div class="footer__widthFanPageFB">
+                        <div class="fb-page" data-href="https://www.facebook.com/facebook" data-tabs="" data-width="" data-height="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/facebook" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/facebook">Facebook</a></blockquote></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer__bottom uk-text-center">
+        <div class="uk-container">
+            <div class="footer__txt">© Bản quyền thuộc về Happy house</div>
+        </div>
+    </div>
+</footer>
+<!--/footer-->
 </div>
 <!--/app-->
 </body>
