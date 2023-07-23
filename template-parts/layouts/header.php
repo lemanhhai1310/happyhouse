5724553de2bb62df2ef2819894fb59c13284bcd4
hailm <hai.lemanh.1310@gmail.com>
2023-07-23 10:10:23 +0700

    feat: update google font, add css

--- template-parts/layouts/header.php
+++ template-parts/layouts/header.php
@@ -11,7 +11,7 @@
     <!--Google Fonts-->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
 
     <!--CSS-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.16.22/dist/css/uikit.min.css" />
